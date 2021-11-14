3a7050de807ed099ebc44fad6621dc2ef1d12371
Nacho_A.inf <87207666+I-AlfaroCor404@users.noreply.github.com>
2021-11-14 15:50:11 -0300

    Update view.py

--- Projecto1.1/Proyecto1/view.py
+++ Projecto1.1/Proyecto1/view.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 from django.template import Template, Context
 
 def saludo(request):
-  Documento_Ext = open("C:/Users/Usuario/OneDrive - Universidad Técnica Federico Santa María/Documentos/GitHub/Covidsearch/Projecto1.1/Proyecto1/plantilas/default.html")
+  Documento_Ext = open("./plantilas/default.html")
   Plantilla = Template(Documento_Ext.read())
   Documento_Ext.close()
   return HttpResponse(Plantilla.render(Context()))
 
   
 def default(request):
-  Documento_Ext = open("C:/Users/Usuario/OneDrive - Universidad Técnica Federico Santa María/Documentos/GitHub/Covidsearch/Projecto1.1/Proyecto1/plantilas/saludo.html")
+  Documento_Ext = open("./plantilas/saludo.html")
   Plantilla = Template(Documento_Ext.read())
   Documento_Ext.close()
   return HttpResponse(Plantilla.render(Context()))
