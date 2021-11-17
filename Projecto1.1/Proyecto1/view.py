91dbd6f729dc69873aecb681d5d725c2ef97f7a9
Nacho_A.inf <87207666+I-AlfaroCor404@users.noreply.github.com>
2021-11-16 22:28:51 -0300

    Funcionamiento del default como corresponde

--- Projecto1.1/Proyecto1/view.py
+++ Projecto1.1/Proyecto1/view.py
@@ -12,7 +12,7 @@ def saludo(request):
   return HttpResponse(Plantilla.render(Context()))
 
 def default(request):
-  with open(d + "/Proyecto1/plantilas/saludo.html") as Documento_Ext:
+  with open(d + "/Proyecto1/plantilas/default.html") as Documento_Ext:
     Plantilla = Template(Documento_Ext.read())
   Documento_Ext.close()
   return HttpResponse(Plantilla.render(Context()))
