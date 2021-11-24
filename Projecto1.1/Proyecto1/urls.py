332fd2a2ec98988cf5d59344ec7232051eefd8cf
Nacho_A.inf <87207666+I-AlfaroCor404@users.noreply.github.com>
2021-11-24 00:02:41 -0300

    Arreglo final - Reestructuramiento de Django.

--- Projecto1.1/Proyecto1/urls.py
+++ Projecto1.1/Proyecto1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.view import 
+from Proyecto1.view import saludo, defecto, default
 
 urlpatterns = [
     path('admin/', admin.site.urls),
