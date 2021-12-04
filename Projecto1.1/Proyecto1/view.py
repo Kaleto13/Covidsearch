6cec0b90d669b75c18132563447e588a5d4cfc9d
FranciscoCarrB <francisco.carrascbug@usm.cl>
2021-12-04 17:14:31 -0300

    Update estadisticas

--- Projecto1.1/Proyecto1/view.py
+++ Projecto1.1/Proyecto1/view.py
@@ -5,7 +5,7 @@ import os
 d = "/".join(os.getcwd().split("\\"))
 
 def estadisticas_generales(request):
-  with open(d + "/Proyecto1/plantilas/estadisticas_general.html") as Documento_Ext:
+  with open(d + "/Proyecto1/plantilas/estadistica_general.html") as Documento_Ext:
     Plantilla = Template(Documento_Ext.read())
   Documento_Ext.close()
   return HttpResponse(Plantilla.render(Context()))
