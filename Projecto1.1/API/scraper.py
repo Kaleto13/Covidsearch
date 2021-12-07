29b14c96dbbbd162afc0abd4cea5bec5885e9364
ipo <juan.domenech@usm.cl>
2021-12-07 00:41:00 -0300

    update del api para archivos CSV

--- Projecto1.1/API/scraper.py
+++ Projecto1.1/API/scraper.py
@@ -89,9 +89,6 @@ def PasoAPaso():
             substracting_days += 1
     raw = req.text
     raw = raw.split('\n')
-    
-
-    #arch = open('PasoAPaso.csv', 'w')
 
     for i in range(len(raw)):
         
@@ -104,20 +101,95 @@ def PasoAPaso():
         x = len(raw[i])
         j += str(raw[i][0]) + ',' + str(raw[i][1]) + ',' + str(raw[i][2]) + ',' + str(raw[i][3]) + ',' + str(raw[i][4]) + ',' + str(raw[i][x-1]) + '\n'
 
-        #print()
-        #arch.write(str(raw[i][0]) + ',' + str(raw[i][1]) + ',' + str(raw[i][2]) + ',' + str(raw[i][3]) + ',' + str(raw[i][4]) + ',' + str(raw[i][x-1]) + '\n')
-
-    #arch.close()
-    
     return j
 
+#DP13 PCD acumulado e informado en el ultimo dia
 
 
+def PCRAcumulado():
+    substracting_days = 0
+    while True:
+        current_date = datetime.today()
+        current_date = current_date - timedelta(days=substracting_days)
+        today_date = current_date.strftime('%Y-%m-%d')
+        url = f"https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto17/PCREstablecimiento.csv"
+        req = get(url)
+
+        if "Not Found" not in req.text:
+            break
+        else:
+            substracting_days += 1
+    raw = req.text
+    raw = raw.split('\n')
+
+    for i in range(len(raw)):
+
+        raw[i] = raw[i].split(',')
+
+    j = ''
+    for i in range(len(raw)-1):
+        x = len(raw[i])
+        j += str(raw[i][0])+','+str(raw[i][1])+','+str(raw[i][x-1])+';'
+    j = j.strip().split(';')
+    for i in range(len(j)):
+        j[i] = j[i].split(',')
+    
+    dac = {}
+    lac = []
+    for l in range(len(j[0])):
+        dac[j[0][l]] = []
+        lac.append(j[0][l])
+    
+    for t in range(len(j)):
+        if t == 0:
+            pass
+        else:
+            for f in range(len(j[t])):
+                dac[lac[f]].append(j[t][f])
+    arch = open('DP17-PCREstablecimiento.csv', 'w')
+    for f in dac:
+        arch.write(f+',')
+        for x in dac[f]:
+            arch.write(x+',')
+        arch.write('\n')
+    arch.close()
+    return True
+
+
+def CasosNuevosCumulativo():
+    substracting_days = 0
+    while True:
+        current_date = datetime.today()
+        current_date = current_date - timedelta(days=substracting_days)
+        today_date = current_date.strftime('%Y-%m-%d')
+        url = f"https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto13/CasosNuevosCumulativo.csv"
+        req = get(url)
+
+        if "Not Found" not in req.text:
+            break
+        else:
+            substracting_days += 1
+    raw = req.text
+    raw = raw.split('\n')
+
+    for i in range(len(raw)):
+
+        raw[i] = raw[i].split(',')
+
+    j = ''
+
+    for i in range(len(raw)-1):
+
+        x = len(raw[i])
+        j += str(raw[i][0]) + ',' + str(raw[i][1]) + ',' + str(raw[i][2]) + ',' + \
+            str(raw[i][3]) + ',' + str(raw[i][4]) + \
+            ',' + str(raw[i][x-1]) + '\n'
+
+    return j
 
 #print(CasosTotalesPorRegion(), '\n')
-print(TotalNacionalesDiarios(), '\n') 
+#print(TotalNacionalesDiarios(), '\n') 
 #print(PCRPorRegion(), '\n')
 #print(PasoAPaso(), '\n')
-
-#necesitamos: Fecha, Casos nuevos totales, casos totales, fallecidos, casos activos confirmados
-#Todo esto parte del producto 5 del github
+#print(PCRAcumulado(), '\n')
+print(CasosNuevosCumulativo(), '\n')
