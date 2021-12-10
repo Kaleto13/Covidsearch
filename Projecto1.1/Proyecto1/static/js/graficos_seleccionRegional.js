70de914bec29da5d241de05c33f9bd5299f4ca39
Nacho_A.inf <87207666+I-AlfaroCor404@users.noreply.github.com>
2021-12-10 18:51:57 -0300

    ??

--- Projecto1.1/Proyecto1/static/js/graficos_seleccionRegional.js
+++ Projecto1.1/Proyecto1/static/js/graficos_seleccionRegional.js
@@ -1,16 +1,33 @@
-var options = {
-    chart: {
-      type: 'line'
-    },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
-  }
-  
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  
-  chart.render();
+google.charts.load('current', {'packages':['corechart']});
+
+
+function drawChart() {
+    var datos = [
+        ['Year', 'Sales'],
+        ['2013',  1000],
+        ['2014',  1170],
+        ['2015',  660],
+        ['2016',  1030]
+    ]
+
+    var data = google.visualization.arrayToDataTable(datos);
+
+    var Titulo;
+    $('.home_content .mapa')[0].addEventListener('load', function() {
+        $('.Arica #AricaPath', this.contentDocument).on({
+            'mouseenter': function() {
+                Titulo = 'Región de Arica y Parinacota';
+            },
+        });
+    }, true);
+
+    var options = {
+        hAxis: {title: 'Fechas',  titleTextStyle: {color: '#333'}},
+        vAxis: {minValue: 0}
+    };
+
+    var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+google.charts.setOnLoadCallback(drawChart);
